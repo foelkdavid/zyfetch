ee9ce4d5f73071ebdaac2c68319cb927473c2b1b
David Fölk <david@foelk.com>
2024-10-09 14:40:02 +0200

    improved documentation

--- src/zyfetch.zig
+++ src/zyfetch.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 
+// Gets GPU Vendor using lspci
 fn getVGAInfo(allocator: std.mem.Allocator) ![]const u8 {
     const result = try std.process.Child.run(.{
         .allocator = allocator,
@@ -13,6 +14,7 @@ fn getVGAInfo(allocator: std.mem.Allocator) ![]const u8 {
     return allocator.dupe(u8, result.stdout);
 }
 
+// Trims Key=Value lines like those in /etc/os-release
 pub fn trimName(allocator: std.mem.Allocator, line: []const u8) ![]const u8 {
     const start = std.mem.indexOfScalar(u8, line, '"') orelse return error.InvalidFormat;
     const end = std.mem.lastIndexOfScalar(u8, line, '"') orelse return error.InvalidFormat;
@@ -22,6 +24,7 @@ pub fn trimName(allocator: std.mem.Allocator, line: []const u8) ![]const u8 {
     return try allocator.dupe(u8, line[start + 1 .. end]);
 }
 
+// Reads a file and returns the first line that starts with <string>
 pub fn readLine(allocator: std.mem.Allocator, filepath: []const u8, starts_with: []const u8) ![]const u8 {
     const file = try std.fs.cwd().openFile(filepath, .{});
     defer file.close();
@@ -38,6 +41,8 @@ pub fn readLine(allocator: std.mem.Allocator, filepath: []const u8, starts_with:
     return error.LineNotFound;
 }
 
+// Returns a defined part of a String.
+// Parts are seperated by whitespaces
 pub fn getLinePart(allocator: std.mem.Allocator, line: []const u8, part_index: usize) ![]const u8 {
     var iter = std.mem.split(u8, line, " ");
     var current_index: usize = 0;
@@ -52,6 +57,8 @@ pub fn getLinePart(allocator: std.mem.Allocator, line: []const u8, part_index: u
     return error.PartNotFound;
 }
 
+// Returns all content after a defined part of a String.
+// Parts are seperated by whitespaces
 pub fn getLinePartAndRest(allocator: std.mem.Allocator, line: []const u8, start_index: usize) ![]const u8 {
     var iter = std.mem.split(u8, line, " ");
     var current_index: usize = 0;
@@ -75,76 +82,79 @@ pub fn getLinePartAndRest(allocator: std.mem.Allocator, line: []const u8, start_
     return result.toOwnedSlice();
 }
 
+// Fetches OS/Distro
 pub fn getOS(allocator: std.mem.Allocator) ![]const u8 {
     const full_line = try readLine(allocator, "/etc/os-release", "PRETTY_NAME=");
     defer allocator.free(full_line);
     return try trimName(allocator, full_line);
 }
 
-//      Host: 21A0000DGE ThinkPad P14s Gen 2a
+// Fetches hostname
 pub fn getHost(allocator: std.mem.Allocator) ![]const u8 {
     const hostname = try readLine(allocator, "/etc/hostname", "");
     return hostname;
 }
 
+// Fetches kernel version
 pub fn getKernel(allocator: std.mem.Allocator) ![]const u8 {
     const kernel_full_line = try readLine(allocator, "/proc/version", "");
     defer allocator.free(kernel_full_line);
     return try getLinePart(allocator, kernel_full_line, 2);
 }
-//      Uptime: 1 day, 12 hours, 55 mins
+
+// Fetches uptime <TODO: implement formatting>
 pub fn getUptime(allocator: std.mem.Allocator) ![]const u8 {
     const uptime_full_line = try readLine(allocator, "/proc/uptime", "");
     defer allocator.free(uptime_full_line);
     return try getLinePart(allocator, uptime_full_line, 0);
 }
 
-//      Packages: 1212 (xbps-query), 16 (flatpak)
+// Fetches number of installed packages.
 pub fn getPackages() []const u8 {
     return "Not Implemented";
 }
-//      Shell: zsh 5.9
+
+// Fetches used shell
 pub fn getShell(allocator: std.mem.Allocator) ![]const u8 {
     const shell = std.posix.getenv("SHELL") orelse return error.ShellNotFound;
     return try allocator.dupe(u8, shell);
 }
 
-//      Resolution: 1920x1080
+// Fetches resolution
 pub fn getResolution() []const u8 {
     return "Not Implemented";
 }
-//      DE: river
-//      WM: sway
+
+// Fetches window manager (if applicable)
 pub fn getWM() []const u8 {
     return "Not Implemented";
 }
 
-//      Terminal: alacritty
+// Fetches terminal
 pub fn getTerm() []const u8 {
     return "Not Implemented";
 }
 
-//      CPU: AMD Ryzen 7 PRO 5850U with Radeon Graphics
-//
+// Fetches CPU
 pub fn getCPU(allocator: std.mem.Allocator) ![]const u8 {
     const cpu_full_line = try readLine(allocator, "/proc/cpuinfo", "model name");
     defer allocator.free(cpu_full_line);
     return try getLinePartAndRest(allocator, cpu_full_line, 2);
 }
 
-//      GPU: AMD ATI Radeon Vega Series / Radeon Vega
+// Fetches GPU
 pub fn getGPU(allocator: std.mem.Allocator) ![]const u8 {
     const vga_full_line = try getVGAInfo(allocator);
     std.debug.print("Line: {s}\n", .{vga_full_line});
     return "Not Implemented";
 }
 
-//      Memory: 13132MiB / 27890MiB
+// Fetches memory stats
 pub fn getMemory() []const u8 {
     return "Not Implemented";
 }
 
-//      Disk: 35G / 458G (8%)
+// Fetches disk stats
 pub fn getDisk() []const u8 {
     return "Not Implemented";
 }
